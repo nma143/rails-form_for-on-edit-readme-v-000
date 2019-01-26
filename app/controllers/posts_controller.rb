7f3315a44d253c1858d2f507bb949fb22ca0db30
Nikki A <nikki.apostolakis@gmail.com>
2019-01-26 20:57:54 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,7 +25,8 @@ class PostsController < ApplicationController
 
 	def update
 	  @post = Post.find(params[:id])
-	  @post.update(title: params[:post][:title], description: params[:post][:description])
+	  # @post.update(title: params[:title], description: params[:description])
+		@post.update(params.require(:post))
 	  redirect_to post_path(@post)
 	end
 end
